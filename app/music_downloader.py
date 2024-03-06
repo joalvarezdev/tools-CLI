aa66099a9d08675a6b43d1abf834e54b98a4b4a7
joalvarez <alvarez.joaquinez@gmail.com>
2024-03-06 19:47:14 -0300

    feat: create method for convert video(mp4) to mp3

--- app/music_downloader.py
+++ app/music_downloader.py
@@ -1,15 +1,57 @@
 from pytube import YouTube
 from app.shared.environments import download_path
 from app.shared.utils import obtain_download_data, rename_file
+from app.shared.file_utils import remove_file
+
+import subprocess
 
 
-def datadownload():
+def __data_download():
     data = obtain_download_data("Enter url and file name")
     youtube = YouTube(data.url)
     try:
         video = youtube.streams.filter(only_audio=True).first()
         out_file = video.download(download_path())
-        rename_file(out_file, f"{download_path()}/{data.name}")
+        file_name = f"{download_path()}/{data.name}"
+
+        rename_file(out_file, file_name)
+
+        video = f"{file_name}.mp4"
+
+        __convert_video_to_mp3(video, f"{file_name}.mp3")
+
+        remove_file(video)
+
     except Exception as e:
         print("Something Went Wrong Please Try Again")
+        print(e)
+
+
+def __convert_video_to_mp3(input_file, output_file):
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-i",
+        input_file,
+        "-vn",
+        "-acodec",
+        "libmp3lame",
+        "-ab",
+        "192k",
+        "-ar",
+        "44100",
+        "-y",
+        output_file,
+    ]
+
+    try:
+        subprocess.run(
+            ffmpeg_cmd,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.DEVNULL,
+        )
+        print("Success")
+    except subprocess.CalledProcessError as e:
+        print("Conversion failed!")
         print(e)
